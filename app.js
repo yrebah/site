1856752b2bdc8219c119f412768ae745b2ac7a75
Yoann <rebah.yoann@gmail.com>
2022-05-27 23:11:23 +0200

    init

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const app = express();
 const port = 3000;
 
 // Static files
-app.use(express.static('public'))
-app.use('/css', express.static(__dirname + '/public/css'))
-app.use('/js', express.static(__dirname + '/public/js'))
-app.use('/img', express.static(__dirname + '/public/img'))
-app.use('/json', express.static(__dirname + '/public/json'))
+app.use(express.static(__dirname + '/public'));
+// app.use('/css', express.static(__dirname + '/public/css'))
+// app.use('/js', express.static(__dirname + '/public/js'))
+// app.use('/img', express.static(__dirname + '/public/img'))
+// app.use('/json', express.static(__dirname + '/public/json'))
 
 // Set Views
 app.set('views', './views')
