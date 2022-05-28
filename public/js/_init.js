c04838cadce0e2b1cbbf4e81ee23dc1c520a9d44
Yoann <rebah.yoann@gmail.com>
2022-05-28 19:57:34 +0200

    init

--- public/js/_init.js
+++ public/js/_init.js
@@ -1,2 +1,42 @@
 // _init.js
 
+$(document).ready(() => {
+    set_defaultTheme()
+    apply_theme()
+    set_btnThemeFunctions()
+})
+
+const apply_theme = () => {
+    if (localStorage.getItem('theme') != null) {
+        switch (localStorage.getItem('theme')) {
+            case 'light':
+                $('#moon').show()
+                $('#sun').hide()
+                break;
+            case 'dark':
+                $('#moon').hide()
+                $('#sun').show()
+                break;
+        }
+    }
+}
+
+const set_defaultTheme = () => {
+    if (localStorage.getItem('theme') == null) {
+        set_theme('light')
+    }
+}
+
+const set_theme = (value) => {
+    localStorage.setItem('theme', value)
+    apply_theme()
+}
+
+const set_btnThemeFunctions = () => {
+    $('#moon').click(() =>{
+        set_theme('dark')
+    })
+    $('#sun').click(() =>{
+        set_theme('light')
+    })
+}
