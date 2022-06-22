93fe66acf87678c0424de094d5049100d452dd67
Yoann <rebah.yoann@gmail.com>
2022-06-22 21:18:07 +0200

    create mainData.js to load main.json data

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import bodyParser from 'body-parser';
 import bcrypt from 'bcrypt';
 import { queries } from "./controller/db.js";
 import { tools } from "./controller/tools.js";
+import { mainData } from "./controller/mainData.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -29,32 +30,20 @@ app.use('/json', express.static(__dirname + '/public/json'));
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-// Data page from main.json
-const main_json = fs.readFileSync('./public/json/main.json');
-const main_json_parsed = JSON.parse(main_json);
-
-const data_site = main_json_parsed.site
-const data_404 = main_json_parsed.page404
-const data_header = main_json_parsed.header
-const data_mainMenu = main_json_parsed.mainMenu
-const data_social = main_json_parsed.social
-const data_footer = main_json_parsed.footer
-const data_searchBar = main_json_parsed.searchBar
-
 let userName = await queries.USER.GetNameById('5')
 userName = userName[0].name
 
 // page Home
 app.get('', (req, res) => {
     res.render('index', {
-        title: `${data_site.title} - Accueil`,
+        title: `${mainData.data_site.title} - Accueil`,
         h1: "Accueil",
-        data_site,
-        data_header,
-        data_mainMenu,
-        data_social,
-        data_footer,
-        data_searchBar,
+        data_site: mainData.data_site,
+        data_header: mainData.data_header,
+        data_mainMenu: mainData.data_mainMenu,
+        data_social: mainData.data_social,
+        data_footer: mainData.data_footer,
+        data_searchBar: mainData.data_searchBar,
         userName
     })
 })
@@ -62,14 +51,14 @@ app.get('', (req, res) => {
 // page About
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: `${data_site.title} - A propos`,
+        title: `${mainData.data_site.title} - A propos`,
         h1: "A propos",
-        data_site,
-        data_header,
-        data_mainMenu,
-        data_social,
-        data_footer,
-        data_searchBar,
+        data_site: mainData.data_site,
+        data_header: mainData.data_header,
+        data_mainMenu: mainData.data_mainMenu,
+        data_social: mainData.data_social,
+        data_footer: mainData.data_footer,
+        data_searchBar: mainData.data_searchBar,
         userName
     })
 })
@@ -77,9 +66,9 @@ app.get('/about', (req, res) => {
 // login
 app.get('/login', (req, res) => {
     res.render('login', {
-        title: `${data_site.title} - Connexion`,
+        title: `${mainData.data_site.title} - Connexion`,
         h1: `Se connecter à ${data_site.title}`,
-        data_site
+        data_site: mainData.data_site
     })
 })
 
@@ -99,9 +88,9 @@ app.post('/login', (req, res) => {
 // register
 app.get('/register', (req, res) => {
     res.render('register', {
-        title: `${data_site.title} - Créer un compte`,
-        h1: `Créer un compte sur ${data_site.title}`,
-        data_site
+        title: `${mainData.data_site.title} - Créer un compte`,
+        h1: `Créer un compte sur ${mainData.data_site.title}`,
+        data_site: mainData.data_site
     })
 })
 
@@ -132,18 +121,18 @@ app.post('/register', async (req, res) => {
 // register-success
 app.get('/register-success', (req, res) => {
     res.render('register-success', {
-        title: `${data_site.title} - Compte créé avec succès !`,
-        h1: `Votre compte ${data_site.title} a été créé avec succès !`,
-        data_site
+        title: `${mainData.data_site.title} - Compte créé avec succès !`,
+        h1: `Votre compte ${mainData.data_site.title} a été créé avec succès !`,
+        data_site: mainData.data_site
     })
 })
 
 // user-already-exist
 app.get('/user-already-exist', (req, res) => {
     res.render('user-already-exist', {
-        title: `${data_site.title} - Erreur lors de la création du compte`,
-        h1: `L'adresse mail renseignée correpond déjà à un compte ${data_site.title}`,
-        data_site
+        title: `${mainData.data_site.title} - Erreur lors de la création du compte`,
+        h1: `L'adresse mail renseignée correpond déjà à un compte ${mainData.data_site.title}`,
+        data_site: mainData.data_site
     })
 })
 
@@ -151,9 +140,9 @@ app.get('/user-already-exist', (req, res) => {
 app.get('/forgot-password', (req, res) => {
 
     res.render('forgot-password', {
-        title: `${data_site.title} - Mot de passe oublié`,
-        h1: `Renouveler votre mot de passe ${data_site.title}`,
-        data_site
+        title: `${mainData.data_site.title} - Mot de passe oublié`,
+        h1: `Renouveler votre mot de passe ${mainData.data_site.title}`,
+        data_site: mainData.data_site
     })
 })
 
@@ -171,9 +160,9 @@ app.post('/forgot-password', (req, res) => {
 // profile
 app.get('/profile', (req, res) => {
     res.render('profile', {
-        title: `${data_site.title} - Détails du compte`,
-        h1: `Détails de votre compte ${data_site.title}`,
-        data_site,
+        title: `${mainData.data_site.title} - Détails du compte`,
+        h1: `Détails de votre compte ${mainData.data_site.title}`,
+        data_site: mainData.data_site,
         userName
     })
 })
@@ -194,9 +183,9 @@ app.post('/profile', (req, res) => {
 // 404
 app.get('*', (req, res) => {
     res.render('404', {
-        title: `${data_site.title} - 404`,
-        data_site,
-        data_404
+        title: `${mainData.data_site.title} - 404`,
+        data_site: mainData.data_site,
+        data_404: mainData.data_404
     })
 })
 
---
+++ controller/mainData.js
@@ -0,0 +1,15 @@
+import fs from 'fs';
+
+// Data page from main.json
+const main_json = fs.readFileSync('./public/json/main.json')
+const main_json_parsed = JSON.parse(main_json)
+
+export const mainData = {
+    data_site : main_json_parsed.site,
+    data_404 : main_json_parsed.page404,
+    data_header : main_json_parsed.header,
+    data_mainMenu : main_json_parsed.mainMenu,
+    data_social : main_json_parsed.social,
+    data_footer : main_json_parsed.footer,
+    data_searchBar : main_json_parsed.searchBar
+}
