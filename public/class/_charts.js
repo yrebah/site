ed7e8c67135b4e6a18325498a3fcf6b925112315
Yoann <rebah.yoann@gmail.com>
2022-07-14 23:07:32 +0200

    charts

--- public/class/_charts.js
+++ public/class/_charts.js
@@ -68,3 +68,31 @@ export class _Charts {
         })
     }
 }
+
+// Example :
+
+// const data = [
+//     {
+//         "value": 220,
+//         "percent": 54,
+//         "label": "Coucou",
+//         "href": "http://www.google.com"
+//     },
+//     {
+//         "value": 140,
+//         "percent": 44,
+//         "label": "Coucou"
+//     },
+//     {
+//         "value": 50,
+//         "percent": 14,
+//         "label": "Coucou"
+//     },
+//     {
+//         "value": 80,
+//         "percent": 64,
+//         "label": "Coucou"
+//     }
+// ]
+
+// Charts.Bar('charts', '#content-chart', data)
